d37d4219a0d408a7525b4828d7e762676afcdcdd
Konstantin Zamaraev <zamaraevk@gmail.com>
2016-07-08 17:23:15 -0400

    create bookshelf, knex, models, collections. config.js

---
+++ bookshelf/config.js
@@ -0,0 +1,34 @@
+var Bookshelf = require('bookshelf');
+var path = require('path');
+
+var db = require('knex')({
+  client: 'sqlite3',
+  connection: {
+    host: '127.0.0.1',
+    user: 'your_database_user',
+    password: 'password',
+    database: 'shortlydb',
+    charset: 'utf8',
+    filename: path.join(__dirname, '../db/shortly.sqlite')
+  }
+});
+
+db.schema.hasTable('urls').then(function(exists) {
+  if (!exists) {
+    db.schema.createTable('urls', function (link) {
+      link.increments('id').primary();
+      link.string('url', 255);
+      link.string('base_url', 255);
+      link.string('code', 100);
+      link.string('title', 255);
+      link.integer('visits');
+      link.timestamps();
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+
+var Bookshelf = require('bookshelf')(db);
+module.exports = Bookshelf;
