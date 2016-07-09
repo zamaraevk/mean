0460045136cd934b13fd332b045e69f300d3755b
Konstantin Zamaraev <zamaraevk@gmail.com>
2016-07-08 20:21:59 -0400

    rrr

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ var express = require('express')
 var app = express()
 var port = process.env.PORT || 8000;
 
-app.use(express.static(__dirname + '/client'));
+app.use(express.static('client'));
 
-// app.get('/', function (req, res) {
-// 	res.sendFile(__dirname + '/client/index.html');
-// });
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/client/index.html');
+});
 
 app.listen(port, function () {
-	console.log(__dirname);
   console.log('Example app listening on port ' + port);
 });
