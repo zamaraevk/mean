b6e6d5b97187f91abd002fd6c062f9209c1640e3
Konstantin Zamaraev <zamaraevk@gmail.com>
2016-07-08 20:03:54 -0400

    working on path to controller

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express')
 var app = express()
 var port = process.env.PORT || 8000;
+console.log(__dirname + '/client');
+app.use(express.static(__dirname + '/client'));
 
-app.use(express.static('client'));
-
-app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/client/index.html');
-});
+// app.get('/', function (req, res) {
+// 	res.sendFile(__dirname + '/client/index.html');
+// });
 
 app.listen(port, function () {
   console.log('Example app listening on port ' + port);
